58b5f91c0c6ef3ee8c109b82a048198a4d29bb63
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-07 20:53:38 +0100

    check if the caracter's Lower

---
+++ Marouane/Exercice2/index.js
@@ -0,0 +1,9 @@
+// voyels: a,e,i,u,y
+// a=1,e=2,i=3,u=4,y=5 if the caracter's Upper A=a*2,E=e*2
+// input aiAe 
+// output 8 the sum of the values of the caracters
+
+// check if the caracter's Lower
+const isVoyelLower=(c)=>{
+    return (c=="a" || c=="e" || c=="i" || c=="u" || c=="y") ? true :false
+}
